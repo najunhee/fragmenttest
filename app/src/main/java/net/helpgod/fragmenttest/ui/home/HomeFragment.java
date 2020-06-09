71ccfa139a6edd3b4e16c921ea89c5cb1d4ed965
najunhee <junidream@gmail.com>
2020-06-10 00:17:07 +0900

    첫번째 fragment 등록

--- app/src/main/java/net/helpgod/fragmenttest/ui/home/HomeFragment.java
+++ app/src/main/java/net/helpgod/fragmenttest/ui/home/HomeFragment.java
@@ -13,10 +13,15 @@ import androidx.fragment.app.Fragment;
 
 public class HomeFragment extends Fragment {
 
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
     }
 }
